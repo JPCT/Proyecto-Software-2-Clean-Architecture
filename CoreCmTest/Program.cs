fdb9c17370254967ab25fd45e3e7eeea37c67cf6
sebitas <juan.1701715778@ucaldas.edu.co>
2020-09-08 08:34:34 -0500

    Cambios en el main

--- CoreCmTest/Program.cs
+++ CoreCmTest/Program.cs
@@ -12,20 +12,24 @@ namespace CoreCmTest
         {
            
 
-            /*Test crea historiaa
+            
+            
+        }
+
+        /*Test crea historiaa
              Se pasan los datos necesarios a la controladora para que empiece a ejecutar los metodos necesarios para
             agregar la nueva historia*/
+        public static void registrarHistoria()
+        {
             Ctrl_RegistrarHistoria controlarHistoria = new Ctrl_RegistrarHistoria();
             DateTime fecha = new DateTime(2020, 08, 08);
             controlarHistoria.registrarHistoria("La historia", fecha, "Que lindo dia", null, "15");
 
 
             string historias = TablaHistoria.ToJSON();
-           
+
 
             Console.WriteLine(historias);
-
-            
         }
     }
 }
